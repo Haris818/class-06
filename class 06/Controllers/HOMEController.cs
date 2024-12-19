8eae1a9bb24b3c3dc48b5fc4a5e026df0765f6ce
nabihakhan34 <biyakhan551@gmail.com>
2024-12-19 16:06:39 +0500

    Add project files.

---
+++ class 06/Controllers/HOMEController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace class_06.Controllers
+{
+    public class HOMEController : Controller
+    {
+        // GET: HOME
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Layout()
+        {
+            return View();
+        }
+        public ActionResult website()
+        {
+            return View();
+        }
+        public ActionResult about()
+        {
+            return View();
+        }
+    }
+}
